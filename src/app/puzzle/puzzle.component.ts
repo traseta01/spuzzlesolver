a6d9907dbc0900b7df034c7ff8008f1440b1d65f
traseta <traseta@gmail.com>
2023-01-05 01:48:18 +0100

    Add shuffle puzzle functionality

--- src/app/puzzle/puzzle.component.ts
+++ src/app/puzzle/puzzle.component.ts
@@ -10,13 +10,18 @@ export class PuzzleComponent implements OnInit {
 
   board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+  configuration = new Set();
+
+  // array of possible indexes on the board to move to
+  currentMoves = [-1, -1, -1, -1]
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 
-  /* function to make a move on the board */
+  /* make a move on the board */
   clickFunction(num: number): void {
     // do nothing if we click empty field
     if (num === 9) {
@@ -55,6 +60,61 @@ export class PuzzleComponent implements OnInit {
     return [left, right, up, down];
   }
 
+
+
+  shuffleBoard(arr: number[]): void {
+    // arr = this.shuffle(arr);
+    // this.board = [4,2,3,6,1,9,7,5,8]
+    // this.board = [1,2,3,4,5,9,7,8,6]
+    // this.board = [1,2,3,4,9,5,7,8,6]
+    // this.board = [9, 2, 3, 1, 4, 5, 7, 8, 6]
+    // this.board = [2, 9, 3, 1, 4, 5, 7, 8, 6]
+    // this.board = [8, 2, 9, 7, 1, 3, 4, 6, 5]
+
+    this.configuration = new Set();
+
+    // this.moveNum = 0;
+
+    let brojac = 300 + this.getRandomInt(1, 3);
+    while (brojac > 0) {
+      let niz: number[] = [];
+      this.currentMoves = this.getPossibleMoves(3, this.board.indexOf(9))
+      this.currentMoves.forEach(el => {
+        if (el >= 0)
+          niz.push(this.currentMoves.indexOf(el));
+      })
+
+      this.board = this.moveNext(this.board, this.shuffle(niz)[0]);
+      brojac--;
+    }
+
+  }
+
+
+  // move to index passed to the function
+  moveNext(numar: number[], move: number) {
+
+    let myClonedArray = [...numar];
+    let indexOfNine = numar.indexOf(9);
+
+    // if move is valid, make a move
+    if (this.currentMoves[move] >= 0)
+      [myClonedArray[this.currentMoves[move]], myClonedArray[indexOfNine]] = [myClonedArray[indexOfNine], myClonedArray[this.currentMoves[move]]];
+
+    // return new configuration
+    return myClonedArray;
+  }
+
+
+  /* Utility functions */
+
+  // return random int from min inclusive, max exclusive interval
+  getRandomInt(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+
   // check if array is sorted
   isSorted(numarr: number[]): boolean {
 
@@ -70,4 +130,23 @@ export class PuzzleComponent implements OnInit {
     return true;
   }
 
+
+  // Fisher-Yates unbiased array shuffle
+  shuffle(array: number[]) {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+  }
 }
